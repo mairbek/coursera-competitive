1d673d647609f83bf1d04806e7f8a8f887b1a5bc
Mairbek Khadikov <mkhadikov@gmail.com>
2020-05-10 20:02:22 -0700

    format

--- maximal_square.cc
+++ maximal_square.cc
@@ -26,7 +26,7 @@ int main(int argc, const char** argv) {
     }
   }
 
-  int64 dp[n+1][n+1];
+  int64 dp[n + 1][n + 1];
 
   for (int i = 0; i <= n; i++) {
     dp[i][0] = 0;
@@ -34,25 +34,21 @@ int main(int argc, const char** argv) {
   for (int j = 0; j <= n; j++) {
     dp[0][j] = 0;
   }
-  
+
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
-      dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] - dp[i][j] + a[i][j];
+      dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j] - dp[i][j] + a[i][j];
     }
   }
-  // for (int i = 0; i <= n; i++) {
-  //   for (int j = 0; j <= n; j++) {
-  //     std::cout << dp[i][j] << " ";
-  //   }
-  //   std::cout << std::endl;
-  // }
+
   int64 result = 0;
-  for (int i = k-1; i < n; i++) {
-    for (int j = k-1; j <n; j++) {
+  for (int i = k - 1; i < n; i++) {
+    for (int j = k - 1; j < n; j++) {
       int a = i - k;
       int b = j - k;
 
-      int64 value = dp[i+1][j+1] - dp[a+1][j+1] - dp[i+1][b+1] + dp[a+1][b+1];
+      int64 value = dp[i + 1][j + 1] - dp[a + 1][j + 1] - dp[i + 1][b + 1] +
+                    dp[a + 1][b + 1];
 
       result = std::max(result, value);
     }
