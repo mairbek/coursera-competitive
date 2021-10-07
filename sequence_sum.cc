91ee8e6c8a47c2c43faabeb5b3a11e2fb91ccd0c
Mairbek Khadikov <mkhadikov@gmail.com>
2021-10-07 06:01:59 -0700

    stuff

--- sequence_sum.cc
+++ sequence_sum.cc
@@ -7,46 +7,36 @@ namespace {
 } // namespace
 
 using int64 = long long;
-int mod_inv(int a, int m) {
-  if (m == 1) {
-    return 0;
-  }
-  int m0 = m;
-  int y = 0, x = 1;
-  while (a > 1) {
-    // q is quotient
-    int q = a / m;
-    int t = m;
 
-    // m is remainder now, process same as
-    // Euclid's algo
-    m = a % m, a = t;
-    t = y;
+int gcd(int a, int b) {
+  if (b > a) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+  }
 
-    // Update y and x
-    y = x - q * y;
-    x = t;
+  while (b > 0) {
+    int temp = a;
+    a = b;
+    b = temp % b;
   }
-  // Make x positive
-  if (x < 0) {
-    x += m0;
-  }
-  return x;
+  return a;
 }
 
-int crt(const std::vector<int>& a, const std::vector<int>& m) {
-  int k = m.size();
-  int prod = 1;
-  for (int i = 0; i < k; i++) {
-    prod *= m[i];
-  }
-  int result = 0;
-  for (int i = 0; i < k; i++) {
-    int pp = prod / m[i];
-    result += a[i] * mod_inv(pp, m[i]) * pp;
-  }
-  return result % prod;
+int phifun(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
 }
+
 
 int mod_mul(int a, int b, int m) {
   int64 t = ((int64) a) * b;
@@ -76,29 +66,6 @@ int mod_pow(int x, int n, int p) {
   return mod_mul(x, y, p);
 }
 
-int pow(int x, int n) {
-  if (n == 0) {
-    return 0;
-  }
-  int y = 1;
-  while (n > 1) {
-    if (n % 2 == 0) {
-      n = n / 2;
-    } else {
-      y = x * y;
-      n = (n - 1) / 2;
-    }
-    x = x * x;
-  }
-  return x * y;
-}
-
-int mod_div(int a, int b, int m) {
-  a = a % m;
-  int inv = mod_inv(b, m);
-  return mod_mul(a, inv, m);
-}
-
 int mod_geosum(int a, int n, int m) {
   if (n == 0) {
     return 1;
@@ -123,74 +90,37 @@ int main(int argc, const char** argv) {
 
   std::cin >> n >> m;
 
-  int mindiv[m+1];
-  for (int i = 0; i <=m; i++) {
-    mindiv[i] = 0;
-  }
-  std::vector<int> primes;
-  for (int i = 2; i <= m; i++) {
-    if (mindiv[i] == 0) {
-      primes.push_back(i);
-      mindiv[i] = i;
+  int pk = m;
+  int phi = phifun(pk);
+  int shift_pow = pk - phi;
+  int sum = 0;
+
+  for (int i = 1; i <= std::min(pk, n); i++) {
+    int ii = mod_pow(i, i, pk);
+    int total_ii = (n-i) / pk + 1;
+    int rings = total_ii / phi;
+    int rem = total_ii % phi;
+
+    if (i == 1) {
+      int ssum = mod_mul(rings, phi, pk);
+      ssum = mod_sum(ssum, rem, pk);
+      sum = mod_sum(sum, ssum, pk);
+      continue;
     }
-    for (int j = 0; j < primes.size(); j++) {
-      int q = primes[j];
-      if (q > mindiv[i] || q * i > m) {
-        break;
-      }
-      mindiv[q * i] = q;
+
+    int igcd = mod_pow(i, shift_pow, pk);
+    int ring_sum = mod_geosum(igcd, phi-1, pk);
+    int rem_sum = 0;
+    if (rem > 0) {
+      rem_sum = mod_geosum(igcd, rem-1, pk);
     }
+
+    int total_sum = mod_mul(ring_sum, rings, pk);
+    total_sum = mod_sum(total_sum, rem_sum, pk);
+    total_sum = mod_mul(total_sum, ii, pk);
+    sum = mod_sum(sum, total_sum, pk);
   }
-  std::unordered_map<int, int> factors;
-  int mm = m;
-  while (mm > 1) {
-    int p = mindiv[mm];
-    if (factors.count(p) == 0) {
-      factors[p] = 0;
-    }
-    factors[p] += 1;
-    mm /= p;
-  }
-
-  std::vector<int> mods;
-  std::vector<int> sums;
-  for (auto const& pf : factors) {
-    int p = pf.first;
-    int k = pf.second;
-    int pk = pow(p, k);
-    int phi = pk - pk/p;
-    mods.push_back(pk);
-    int sum = 0;
-
-    for (int i = 1; i <= std::min(pk, n); i++) {
-      int ii = mod_pow(i, i, pk);
-      int total_ii = (n-i) / pk + 1;
-      int rings = total_ii / phi;
-      int rem = total_ii % phi;
-
-      if (i == 1) {
-        int ssum = mod_mul(rings, phi, pk);
-        ssum = mod_sum(ssum, rem, pk);
-        sum = mod_sum(sum, ssum, pk);
-        continue;
-      }
-      int ring_sum = mod_geosum(i, phi-1, pk);
-      int rem_sum1 = 0;
-      if (rem > 0) {
-        rem_sum1 = mod_geosum(i, rem-1, pk);
-      }
-
-      int total_sum = mod_mul(ring_sum, rings, pk);
-      total_sum = mod_sum(total_sum, rem_sum1, pk);
-      total_sum = mod_mul(total_sum, ii, pk);
-      sum = mod_sum(sum, total_sum, pk);
-    }
-    sums.push_back(sum);
-    std::cout << pk << " " << sum << std::endl;
-  }
-  int r = crt(sums, mods);
-  std::cout << r << std::endl;
-
+  std::cout << sum << std::endl;
   return 0;
 }
 
